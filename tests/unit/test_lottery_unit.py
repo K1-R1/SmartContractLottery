d91ab5cc109920982eb32095cd815a41b0f367e9
KR <kyranrawlinson@gmail.com>
2022-02-09 00:04:38 +0000

    Updated tests/unit/test_lottery_unit.py
    Added unit test: cant_enter_unless_started

--- tests/unit/test_lottery_unit.py
+++ tests/unit/test_lottery_unit.py
@@ -1,13 +1,14 @@
-from brownie import web3
+from brownie import exceptions
 from scripts.deploy_lottery import deploy_lottery
+from scripts.general_scripts import get_account
 from web3 import Web3
 from brownie import network, config
 import pytest
 
 def test_get_entrance_fee():
+    #Arrange
     if not config['networks'][network.show_active()]['local'] is True:
         pytest.skip()
-    #Arrange
     lottery = deploy_lottery()
     #Act
     entrance_fee = lottery.getEntranceFee().return_value
@@ -17,3 +18,12 @@ def test_get_entrance_fee():
     expected_entrance_fee = Web3.toWei(0.0125, 'ether')
     #Assert
     assert entrance_fee == expected_entrance_fee
+
+def test_cant_enter_unless_started():
+    #Arrage
+    if not config['networks'][network.show_active()]['local'] is True:
+        pytest.skip()
+    lottery = deploy_lottery()
+    #Act / Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({'from': get_account(), 'value': lottery.getEntranceFee().return_value})
